0c325fafd59b3aa0ce46a282e6fefe1922ff7080
kerry-jalal <khariy34@hotmail.com>
2024-03-19 20:50:45 +0100

    empty test

---
+++ _test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateCar(t *testing.T) {
+	// Define test cases
+	assert.True(t, true, "True is true!")
+}
