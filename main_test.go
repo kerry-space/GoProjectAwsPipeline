d14504d764369ad0d626fd0fc327970b1f517141
kerry-jalal <khariy34@hotmail.com>
2024-03-21 14:46:35 +0100

    update test file

--- main_test.go
+++ main_test.go
@@ -1,10 +1,13 @@
-package main_test
+package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateCar(t *testing.T) {
 	// Define test cases
+	assert.True(t, true, "True is true!")
 	//t.Fatal("Failed")
 }
