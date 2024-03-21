21777486323cb4712dfc444f2eb3eea8ae265d2f
kerry-jalal <khariy34@hotmail.com>
2024-03-21 14:15:04 +0100

    udpate the buildspec.yaml to push to dockerhub instead

--- main_test.go
+++ main_test.go
@@ -2,11 +2,9 @@ package main_test
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateCar(t *testing.T) {
 	// Define test cases
-	assert.True(t, true, "True is true!")
+	//t.Fatal("Failed")
 }
